7efab6e839497593c0bd608e2f696607fcc60a1b
asthabansal00 <138471247+asthabansal00@users.noreply.github.com>
2023-10-04 03:31:39 +0530

    getData function added
    to avoid display error while adding the data

--- app/src/main/java/com/asthabansal/firebasefirestore/MainActivity.kt
+++ app/src/main/java/com/asthabansal/firebasefirestore/MainActivity.kt
@@ -13,6 +13,7 @@ import com.asthabansal.firebasefirestore.databinding.CustomUpdateDialogBinding
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
+import java.lang.reflect.Array.get
 
 class MainActivity : AppCompatActivity(),RecyclerInterface {
     var notesList = arrayListOf<NotesDataClass>()
@@ -32,28 +33,18 @@ class MainActivity : AppCompatActivity(),RecyclerInterface {
         adapter = RecyclerAdapter(notesList, this )
         binding.recycler.adapter = adapter
 
-        firestore.collection("User")
-            .get()
-            .addOnSuccessListener {
-                System.out.println("in snapshot ${it.documents}")
-                for( items in it.documents){
-                    System.out.println("items ${items.data}")
-                    var firestoreClass = items.toObject<NotesDataClass>()?: NotesDataClass()
-                    firestoreClass.id = items.id
-                    notesList.add(firestoreClass)
-                }
-                
-                adapter.notifyDataSetChanged()
-            }
+
+
         binding.fab.setOnClickListener {
             var dialog = Dialog(this)
             var dialogbinding = CustomDialogBinding.inflate(layoutInflater)
             dialog.window?.setLayout(
                 ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT
-            )
+                ViewGroup.LayoutParams.MATCH_PARENT)
+
             dialog.show()
             dialog.setContentView(dialogbinding.root)
+
             dialogbinding.btnAdd.setOnClickListener {
                 if (dialogbinding.etTitle.text.toString().isEmpty()) {
                     dialogbinding.etTitle.error = "Enter your name"
@@ -61,11 +52,11 @@ class MainActivity : AppCompatActivity(),RecyclerInterface {
                     dialogbinding.etDescription.error = "Enter your roll no"
                 } else {
                     firestore.collection("User")
-                        .add(
-                        NotesDataClass(title = dialogbinding.etTitle.text.toString(),
+                        .add(NotesDataClass(title = dialogbinding.etTitle.text.toString(),
                     description = dialogbinding.etDescription.text.toString()))
                         .addOnSuccessListener {
                             Toast.makeText(this@MainActivity,"data added",Toast.LENGTH_LONG).show()
+                            getData()
                         }
                         .addOnCanceledListener {
                             Toast.makeText(this@MainActivity,"Cancelled",Toast.LENGTH_LONG).show()
@@ -87,13 +78,10 @@ class MainActivity : AppCompatActivity(),RecyclerInterface {
             .document(notesList[position].id?:"")
             .delete()
             .addOnSuccessListener {
-                Toast.makeText(this,"data deleted",Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "data deleted", Toast.LENGTH_LONG).show()
                 notesList.removeAt(position)
                 adapter.notifyDataSetChanged()
             }
-
-        //notesList.removeAt(position)
-        //adapter.notifyDataSetChanged()
     }
 
     override fun btnupdate(position: Int) {
@@ -111,22 +99,37 @@ class MainActivity : AppCompatActivity(),RecyclerInterface {
             } else if (dialogbinding.etDescription.text.toString().isEmpty()) {
                 dialogbinding.etDescription.error = "Enter your roll no"
             } else {
-                var updateItem = NotesDataClass(title = dialogbinding.tvTitle.text.toString(),
-                description = dialogbinding.tvDescription.text.toString(),
+                var updateItem = NotesDataClass(title = dialogbinding.etTitle.text.toString(),
+                description = dialogbinding.etDescription.text.toString(),
                 id = notesList[position].id?:"")
                 firestore.collection("User")
                     .document(notesList[position].id?:"")
                     .set(updateItem)
                     .addOnSuccessListener {
                         Toast.makeText(this,"item updated",Toast.LENGTH_LONG).show()
-                        dialog.dismiss()
-                        adapter.notifyDataSetChanged()
                     }
+                dialog.dismiss()
+                adapter.notifyDataSetChanged()
                notesList.set(position,
                     NotesDataClass(
                         title = dialogbinding.etTitle.text.toString(),
                         description = dialogbinding.etDescription.text.toString()))
             }
         }
+    }
+    fun getData(){
+    firestore.collection("User")
+    .get()
+    .addOnSuccessListener {
+        System.out.println("in snapshot ${it.documents}")
+        for( items in it.documents){
+            System.out.println("items ${items.data}")
+            var firestoreClass = items.toObject<NotesDataClass>()?: NotesDataClass()
+            //items.id = firestoreClass.id
+            firestoreClass.id = items.id
+            notesList.add(firestoreClass)
+            adapter.notifyDataSetChanged()
+        }
+    }
     }
 }
